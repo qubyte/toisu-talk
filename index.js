fb5ff33e5b1ede33a3f7d65b0d7d1d73abf1795a
Mark S. Everitt <mark.s.everitt@gmail.com>
2015-12-02 19:24:59 +0000

    Migrate to TOISU.

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 'use strict';
 
-const express = require('express');
+const Toisu = require('toisu');
+const ToisuRouter = require('toisu-router');
+const fs = require('fs');
+const path = require('path');
 const http = require('http');
 const nudge = require('nudge');
-const bodyParser = require('body-parser');
 const EventEmitter = require('events');
 
-const app = express();
 const navEmitter = new EventEmitter();
 const navEmitterRelay = nudge(navEmitter, {navigate: true, heartbeat: true});
 
 setInterval(() => navEmitter.emit('heartbeat'), 30000);
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
+const router = new ToisuRouter();
 
-app.get('/emitter', navEmitterRelay);
+const indexHtml = fs.readFileSync(path.join(__dirname, 'public', 'index.html'));
+const indexJs = fs.readFileSync(path.join(__dirname, 'public', 'index.js'));
 
-app.post('/navigate', (req, res) => {
-  navEmitter.emit('navigate', {by: req.body.by});
-  res.status(204).end();
+router.add('get', '/', function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': indexHtml.length});
+  res.end(indexHtml);
 });
 
-http.createServer(app).listen(process.env.PORT || 3000, () => console.log('listening'));
+router.add('get', '/index.js', function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/javascript', 'Content-Length': indexJs.length});
+  res.end(indexJs);
+});
+
+router.add('get', '/emitter', navEmitterRelay);
+
+const app = new Toisu();
+
+app.use(router.middleware);
+
+http.createServer(app.requestHandler).listen(process.env.PORT || 3000, () => console.log('listening'));
